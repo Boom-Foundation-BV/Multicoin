050518f8cb99cb95dffd1edae00afbed26378b20
MohamedLEGH <mohamed.amine.legheraba@gmail.com>
2018-08-14 22:06:37 +0200

    minor changes

--- contracts/Multicoin.sol
+++ contracts/Multicoin.sol
@@ -3,12 +3,17 @@ pragma solidity ^0.4.24;
 
 import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 import 'openzeppelin-solidity/contracts/token/ERC20/BurnableToken.sol';
+import 'openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
 
 /**
  * Multiven Token : Multicoin
+ * ERC20 token + Ownable + Burnable
+ * author : Mohamed Amine LEGHERABA
+ * Multiven Group
+ * https://www.multiven.io/
 **/
 
-contract Multicoin is Ownable,BurnableToken {
+contract Multicoin is Ownable,BurnableToken,StandardToken {
   
   string public constant name="Multicoin";
   string public constant symbol="MTC";
