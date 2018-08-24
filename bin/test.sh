072be668d4e6dd7d399b339a8d80fc09b340aeff
MohamedLEGH <mohamed.amine.legheraba@gmail.com>
2018-08-24 08:30:40 +0200

    modif travis settings

--- bin/test.sh
+++ bin/test.sh
@@ -2,10 +2,8 @@
  
 set -e
  
-ganache-cli --gasLimit 5612388 2> /dev/null 1> /dev/null &
+ganache-cli 2> /dev/null 1> /dev/null &
 sleep 5 # to make sure ganache-cli is up and running before compiling
 rm -rf build
-truffle compile
-truffle migrate --reset --network development --multiven_address "0x23F924D8E066d2733761ab5CBd7BAf53449c7eA1"
 truffle test
 kill -9 $(lsof -t -i:8545)
