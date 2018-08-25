df7bcbe8f2aa7f72d5bf6363caeb2645f6d0725e
MohamedLEGH <mohamed.amine.legheraba@gmail.com>
2018-08-25 13:09:41 +0200

    fix coveralls

--- bin/travis-install.sh
+++ bin/travis-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 npm install
 npm install -g ganache-cli truffle
+npm install -g coveralls
 npm install --save-dev solidity-coverage
 npm install -E openzeppelin-solidity
 npm install truffle-hdwallet-provider
